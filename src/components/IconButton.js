2819a33a805273a832c6b9f934e849a060424db6
Noor <noormahammad@users.noreply.github.com>
2020-03-21 10:41:03 -0400

    rotation animation

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,29 +1,59 @@
 import React, { Component } from "react";
-import { View, TouchableWithoutFeedback } from "react-native";
+import { View, TouchableWithoutFeedback, Animated, Easing } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import AnimatedModal from "./AnimatedModal";
 
 const icon_color = "#586069";
 const icon_size = 15;
 
 type Props = {};
 export default class IconButton extends Component<Props> {
+  constructor(props) {
+    super(props);
+    //declare animated value
+    this.rotateValue = new Animated.Value(0);
+  }
   render() {
     const { icon, onPress, data } = this.props;
+    
+    //specifiy how the animated value will change over time
+    let rotation = this.rotateValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"] //degree of rotation
+    });
+
+    //declare animated style
+    let transformStyle = { transform: [{ rotate: rotation }] };
 
     return (
       <TouchableWithoutFeedback
-        onPress={() => {
+        onPressIn={() => {
+          //start animation
+          Animated.timing(this.rotateValue, {
+            toValue: 1, 
+            duration: 700,
+            easing: Easing.linear      
+          }).start();
           onPress(data);
         }}
+
+        onPressOut={()=>{
+          //start animation
+          Animated.timing(this.rotateValue, {
+            toValue: 0, 
+            duration: 350, 
+            easing: Easing.linear
+          }).start();
+        }}
       >
-        <View>
+        <Animated.View style={transformStyle}>
           <Icon
             name={icon}
             style={styles.icon}
             size={icon_size}
             color={icon_color}
           />
-        </View>
+        </Animated.View>
       </TouchableWithoutFeedback>
     );
   }
